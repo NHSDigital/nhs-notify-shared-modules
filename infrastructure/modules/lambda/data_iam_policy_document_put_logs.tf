55def837e8eec7d4e3488c232299ff3a2d865dd2
aidenvaines-bjss <aiden.vaines@bjss.com>
2024-10-11 13:12:34 +0100

    CCM-7006 fixing IAM policy for lambda@edge

--- infrastructure/modules/lambda/data_iam_policy_document_put_logs.tf
+++ infrastructure/modules/lambda/data_iam_policy_document_put_logs.tf
@@ -24,14 +24,19 @@ data "aws_iam_policy_document" "put_logs" {
       effect = "Allow"
 
       actions = [
+        "logs:CreateLogGroup",
         "logs:CreateLogStream",
         "logs:PutLogEvents",
-        "logs:CreateLogGroup",
       ]
 
       resources = [
         format(
           "arn:aws:logs:us-east-1:%s:log-group:/aws/lambda/%s:*",
+          var.aws_account_id,
+          var.function_name,
+        ),
+        format(
+          "arn:aws:logs:*:%s:log-group:/aws/lambda/us-east-1.%s:*",
           var.aws_account_id,
           var.function_name,
         )
