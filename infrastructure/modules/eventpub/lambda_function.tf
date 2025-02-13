b5f6de496931ff7ffc16d9f3d0377bf087973009
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-02-13 09:19:14 +0000

    CCM-7938 Dependency fix

--- infrastructure/modules/eventpub/lambda_function.tf
+++ infrastructure/modules/eventpub/lambda_function.tf
@@ -9,8 +9,8 @@ resource "aws_lambda_function" "main" {
   memory_size = 128
   timeout     = 20
 
-  filename         = data.archive_file.lambda.output_path
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  filename         = archive_file.lambda.output_path
+  source_code_hash = archive_file.lambda.output_base64sha256
 
   logging_config {
     application_log_level = var.log_level
@@ -27,6 +27,4 @@ resource "aws_lambda_function" "main" {
       THROTTLE_DELAY_MS           = "0"
     }
   }
-
-  depends_on = [ data.archive_file.lambda ] # This should not be needed but it is
 }
