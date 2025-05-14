f4c539874862f6912ce5a383d65c7605f3bc1f4d
sidnhs <siddharth.nair1@nhs.net>
2025-05-14 13:44:39 +0100

    CCM-9868: Making var name consistent

--- infrastructure/modules/lambda/cloudwatch_log_subscription_filter_firehose.tf
+++ infrastructure/modules/lambda/cloudwatch_log_subscription_filter_firehose.tf
@@ -3,6 +3,6 @@ resource "aws_cloudwatch_log_subscription_filter" "firehose" {
   name            = replace(aws_cloudwatch_log_group.main.name, "/", "-")
   log_group_name  = aws_cloudwatch_log_group.main.name
   filter_pattern  = var.filter_pattern
-  destination_arn = var.cloudwatch_log_destination_arn
+  destination_arn = var.log_destination_arn
   role_arn        = var.log_subscription_role_arn
 }
