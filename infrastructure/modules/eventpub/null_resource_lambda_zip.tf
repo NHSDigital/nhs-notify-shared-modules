6ffeb0b4f1ccb2107b61a6a8b5d471426ce27d24
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-03 11:28:46 +0100

    CCM-10257: Implement Eventpub in Core

--- infrastructure/modules/eventpub/null_resource_lambda_zip.tf
+++ infrastructure/modules/eventpub/null_resource_lambda_zip.tf
@@ -1,13 +1,12 @@
 resource "null_resource" "lambda_zip" {
+  triggers = {
+    always_run = timestamp()
+  }
   provisioner "local-exec" {
     command = <<EOT
       cd "${path.module}/lambda/eventpub/src" && \
       zip -r ../eventpub.zip . \
         -x "**/__tests__/*" "**/node_modules/*" "**/package.json" "**/package-lock.json"
     EOT
-  }
-
-  triggers = {
-    always_run = timestamp()
   }
 }
