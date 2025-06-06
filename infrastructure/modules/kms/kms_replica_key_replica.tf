6aedaf0712fe3920de439082e4494d3cc6132deb
sidnhs <siddharth.nair1@nhs.net>
2025-06-06 14:38:17 +0100

    CCM-10231: Adding multi-region kms key support

--- infrastructure/modules/kms/kms_replica_key_replica.tf
+++ infrastructure/modules/kms/kms_replica_key_replica.tf
@@ -3,5 +3,6 @@ resource "aws_kms_replica_key" "replica" {
   provider                = aws.us-east-1
   description             = "Multi-Region replica key"
   deletion_window_in_days = var.deletion_window
+  policy                  = data.aws_iam_policy_document.key.json
   primary_key_arn         = aws_kms_key.main.arn
 }
