9f8152c08b2fc70e81a5fef8ce3276c94cbb4f3b
sidnhs <siddharth.nair1@nhs.net>
2025-06-09 10:04:08 +0100

    CCM-10231: Review comments

--- infrastructure/modules/kms/kms_replica_key_replica.tf
+++ infrastructure/modules/kms/kms_replica_key_replica.tf
@@ -1,7 +1,7 @@
 resource "aws_kms_replica_key" "replica" {
   provider                = aws.us-east-1
   count                   = var.is_multi_region ? 1 : 0
-  
+
   description             = "Multi-Region replica key"
   deletion_window_in_days = var.deletion_window
   policy                  = data.aws_iam_policy_document.key.json
