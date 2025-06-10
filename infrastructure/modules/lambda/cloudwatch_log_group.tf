d9b3cff5c550841800878b86c97dea2c92cb6e32
sidnhs <siddharth.nair1@nhs.net>
2025-06-10 14:06:17 +0100

    CCM-10246: Edge lambda regional log group

--- infrastructure/modules/lambda/cloudwatch_log_group.tf
+++ infrastructure/modules/lambda/cloudwatch_log_group.tf
@@ -10,3 +10,17 @@ resource "aws_cloudwatch_log_group" "main" {
     },
   )
 }
+
+resource "aws_cloudwatch_log_group" "main_edge" {
+  count = var.lambda_at_edge ? 1 : 0
+  name              = "/aws/lambda/us-east-1.${local.csi}"
+  retention_in_days = var.log_retention_in_days
+  kms_key_id        = var.kms_key_arn
+
+  tags = merge(
+    local.default_tags,
+    {
+      Name = local.csi
+    },
+  )
+}
