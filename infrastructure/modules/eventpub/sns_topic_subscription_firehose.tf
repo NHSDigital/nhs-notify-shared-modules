458e5f6d421a37a55cf940e96c7c909cda91f5c9
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-13 14:17:16 +0100

    CCM-10258: Discrete Control and Data SNS Topics

--- infrastructure/modules/eventpub/sns_topic_subscription_firehose.tf
+++ infrastructure/modules/eventpub/sns_topic_subscription_firehose.tf
@@ -1,7 +1,7 @@
 resource "aws_sns_topic_subscription" "firehose" {
   for_each = var.enable_event_cache ? local.sns_topics : {}
 
-  topic_arn             = aws_sns_topic[each.key].arn
+  topic_arn             = aws_sns_topic.main[each.key].arn
   protocol              = "firehose"
   subscription_role_arn = aws_iam_role.sns_role.arn
   endpoint              = aws_kinesis_firehose_delivery_stream.main[0].arn
