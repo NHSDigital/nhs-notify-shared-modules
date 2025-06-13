e17387fdf03a62318ccdf7e039cfc8ed00c9627b
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-06-13 14:27:41 +0100

    CCM-10258: Discrete Control and Data SNS Topics

--- infrastructure/modules/eventpub/cloudwatch_log_group_sns_delivery_logging_success.tf
+++ infrastructure/modules/eventpub/cloudwatch_log_group_sns_delivery_logging_success.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_log_group" "sns_delivery_logging_success" {
 
   # SNS doesn't allow specifying a log group and is derived as: sns/${region}/${account_id}/${name_of_sns_topic}/Failure
   # (for failure logs)
-  name = "sns/${var.region}/${var.aws_account_id}/${local.csi}${contains(each.key, "data") ? "-data" : "-control"}"
+  name = "sns/${var.region}/${var.aws_account_id}/${local.csi}${each.key == "data" ? "-data" : "-control"}"
   kms_key_id        = var.kms_key_arn
   retention_in_days = var.log_retention_in_days
 }
