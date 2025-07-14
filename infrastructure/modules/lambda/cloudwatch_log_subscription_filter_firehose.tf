fb8d9174629ad8acb6c2e15667fa49b1f15b628f
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-07-14 15:25:56 +0100

    CCM-11097: Event Validation

--- infrastructure/modules/lambda/cloudwatch_log_subscription_filter_firehose.tf
+++ infrastructure/modules/lambda/cloudwatch_log_subscription_filter_firehose.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_subscription_filter" "firehose" {
   count           = var.send_to_firehose ? 1 : 0 # Keeping this optional for now as don't want to break all lambdas using this, can make this mandatory later
-  name            = replace(aws_cloudwatch_log_group.main.name, "/", "-")
+  name            = trim(replace(aws_cloudwatch_log_group.main.name, "/", "-"), "-")
   log_group_name  = aws_cloudwatch_log_group.main.name
   filter_pattern  = var.filter_pattern
   destination_arn = var.log_destination_arn
