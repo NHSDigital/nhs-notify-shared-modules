c287160f4dea6e2330e95118f88f5873cb3ea7b4
sidnhs <siddharth.nair1@nhs.net>
2025-09-08 17:18:59 +0100

    CCM-11751: Fixing eventpub path

--- infrastructure/modules/eventpub/archive_file_lambda.tf
+++ infrastructure/modules/eventpub/archive_file_lambda.tf
@@ -1,9 +1,9 @@
 resource "archive_file" "lambda" {
   type       = "zip"
-  source_dir = "."
+  source_dir = "${path.root}/.terraform/modules/eventpub/lambda/eventpub/src"
 
   # Timestamp in path to resolve https://github.com/hashicorp/terraform-provider-archive/issues/39
-  output_path = ".${timestamp()}.zip"
+  output_path = "${path.root}/.terraform/modules/eventpub/lambda/eventpub_${timestamp()}.zip"
   excludes = [
     # NodeJS Exclusions
     "**/__tests__",
