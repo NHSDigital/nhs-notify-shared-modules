d3683289103c3fd2ba048ab5aaddc18b5a8441e7
jamesthompson26-nhs <james.thompson26@nhs.net>
2025-09-08 10:43:05 +0100

    CCM-11875: SSL Module Parameter Path Improvements

--- infrastructure/modules/ssl/ssm_parameter_ca_key.tf
+++ infrastructure/modules/ssl/ssm_parameter_ca_key.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "ca_key" {
-  name  = format("/%s/%s/${local.module}/ca-key", var.project, var.environment)
+  name  = format("/%s/%s/%s/${local.module}/ca-key", var.project, var.component, var.environment)
   type  = "SecureString"
   value = tls_private_key.ca_key.private_key_pem
 
--- infrastructure/modules/ssl/ssm_parameter_server_crt.tf
+++ infrastructure/modules/ssl/ssm_parameter_server_crt.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "server_crt" {
-  name  = format("/%s/%s/${local.module}/server-crt", var.project, var.environment)
+  name  = format("/%s/%s/%s/${local.module}/server-crt", var.project, var.component, var.environment)
   type  = "SecureString"
   value = tls_locally_signed_cert.integration_testing_client_cert.cert_pem
 
