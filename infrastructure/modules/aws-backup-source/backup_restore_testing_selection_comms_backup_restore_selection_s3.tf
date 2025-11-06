72045da83e77f110e56c4f563ab9ab85b55e3345
sidnhs <siddharth.nair1@nhs.net>
2025-11-06 13:35:50 +0000

    CCM-12323: Fix restore plan name

--- infrastructure/modules/aws-backup-source/backup_restore_testing_selection_comms_backup_restore_selection_s3.tf
+++ infrastructure/modules/aws-backup-source/backup_restore_testing_selection_comms_backup_restore_selection_s3.tf
@@ -1,9 +1,10 @@
 resource "aws_backup_restore_testing_selection" "backup_restore_testing_selection_s3" {
-  count = var.backup_plan_config_s3.enable ? 1 : 0
-  name                      = "${local.csi_underscore}_s3_backup_restore"
-  restore_testing_plan_name = "${local.csi_underscore}_restore_testing_plan"
+  count                     = var.backup_plan_config_s3.enable ? 1 : 0
+  name                      = "backup_restore_testing_selection_s3"
+  restore_testing_plan_name = local.csi_underscore
   protected_resource_type   = "S3"
   iam_role_arn              = aws_iam_role.backup.arn
+  protected_resource_arns   = ["*"]
   protected_resource_conditions {
     string_equals {
       key   = "aws:ResourceTag/${var.backup_plan_config_s3.selection_tag}"
