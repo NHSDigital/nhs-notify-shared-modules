9cc596917786e68bf9d9febb5f3a6cf9c2bc1e26
Mark Ramsden <mark.ramsden5@nhs.net>
2026-05-08 11:51:37 +0100

    CCM-15212: update eventpub module

--- infrastructure/terraform/modules/eventpub/module_sqs_queue.tf
+++ infrastructure/terraform/modules/eventpub/module_sqs_queue.tf
@@ -1,15 +1,16 @@
 module "sqs_queue" {
   source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/3.1.3/terraform-sqs.zip"
 
-  aws_account_id      = var.aws_account_id
-  component           = var.component
-  environment         = var.environment
-  project             = var.project
-  region              = var.region
-  name                = local.csi
-  create_dlq          = true
-  sqs_kms_key_arn     = var.kms_key_arn
-  sqs_policy_overload = data.aws_iam_policy_document.allow_sns_send.json
+  aws_account_id            = var.aws_account_id
+  component                 = var.component
+  environment               = var.environment
+  project                   = var.project
+  region                    = var.region
+  name                      = local.csi
+  create_dlq                = true
+  sqs_kms_key_arn           = var.sqs_kms_key_arn
+  sqs_policy_overload       = data.aws_iam_policy_document.allow_sns_send.json
+  message_retention_seconds = 1209600 # 14 days
 }
 
 data "aws_iam_policy_document" "allow_sns_send" {
