098defea574f0b6d046a09acbcb2e262b1c37906
aidenvaines-cgi <aiden.vaines@cgi.com>
2026-07-07 12:00:10 +0100

    CCM-17525: Updating grype checks

--- scripts/githooks/scan-dependencies.sh
+++ scripts/githooks/scan-dependencies.sh
@@ -10,6 +10,5 @@ cd "$(git rev-parse --show-toplevel)"
 export BUILD_DATETIME="${BUILD_DATETIME:-$(date -u +'%Y-%m-%dT%H:%M:%S%z')}"
 export GRYPE_FAIL_ON_SEVERITY="${GRYPE_FAIL_ON_SEVERITY:-high}"
 
-"${TOOLING_ROOT}/scripts/reports/create-sbom-report.sh"
 "${TOOLING_ROOT}/scripts/reports/scan-vulnerabilities.sh"
 "${TOOLING_ROOT}/scripts/reports/parse-vulnerabilities.sh" vulnerabilities-repository-report.json
