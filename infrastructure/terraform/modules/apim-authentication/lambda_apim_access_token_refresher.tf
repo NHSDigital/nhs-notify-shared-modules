b4322c6bc851df0ce6462868a9e2c7158160b773
simonlabarere <simon.labarere1@nhs.net>
2026-08-06 10:24:23 +0100

    CCM-21858: Fix lambda dist paths

--- infrastructure/terraform/modules/apim-authentication/lambda_apim_access_token_refresher.tf
+++ infrastructure/terraform/modules/apim-authentication/lambda_apim_access_token_refresher.tf
@@ -19,7 +19,7 @@ module "lambda_lambda_apim_refresh_token" {
   }
 
   function_s3_bucket      = local.acct.s3_buckets["lambda_function_artefacts"]["id"]
-  function_code_base_path = "./dist"
+  function_code_base_path = "${path.module}/dist"
   function_code_dir       = "apim-access-token-refresher"
   function_include_common = true
   handler_function_name   = "handler"
