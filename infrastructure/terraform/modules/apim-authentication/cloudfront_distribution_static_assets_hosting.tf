5e57a545eb92d5c779758b86e1259f44574b21be
simonlabarere <simon.labarere1@nhs.net>
2026-08-06 09:37:21 +0100

    CCM-21858: Remove cloudfront logging config

--- infrastructure/terraform/modules/apim-authentication/cloudfront_distribution_static_assets_hosting.tf
+++ infrastructure/terraform/modules/apim-authentication/cloudfront_distribution_static_assets_hosting.tf
@@ -20,12 +20,6 @@ resource "aws_cloudfront_distribution" "static_assets_hosting" {
     ssl_support_method             = "sni-only"
   }
 
-  logging_config {
-    include_cookies = false
-    bucket          = var.logging_config_s3_bucket
-    prefix          = "${local.csi}/static-assets/"
-  }
-
   origin {
     domain_name = module.s3bucket_static_assets.bucket_regional_domain_name
     origin_id   = "${local.csi}-origin-static-assets"
