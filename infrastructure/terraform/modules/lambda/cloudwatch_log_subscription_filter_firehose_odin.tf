207d922e98535fd1e0e0f824b3ee251cf823aca1
sidnhs <siddharth.nair1@nhs.net>
2026-09-23 10:56:31 +0100

    CCM-23223: Odin logging optional

--- infrastructure/terraform/modules/lambda/cloudwatch_log_subscription_filter_firehose_odin.tf
+++ infrastructure/terraform/modules/lambda/cloudwatch_log_subscription_filter_firehose_odin.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_subscription_filter" "firehose_odin" {
-  count           = var.send_to_firehose ? 1 : 0
+  count           = var.send_to_firehose && var.odin_log_destination_arn != "" ? 1 : 0
   name            = trim(replace(aws_cloudwatch_log_group.main.name, "/", "-"), "-")
   log_group_name  = aws_cloudwatch_log_group.main.name
   filter_pattern  = var.filter_pattern
